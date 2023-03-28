2ffccad666d9719ecc3ceb64d1328d20191413e3
aureate0116 <aureate0116@gmail.com>
2023-03-28 17:11:02 +0800

    deploy

--- assets/SignupView-2b53a85e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-20972b36.js";const r={};function c(n,t){return"註冊頁"}const o=e(r,[["render",c]]);export{o as default};
---
+++ assets/SignupView-bc2faa0e.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-1aad7154.js";const r={};function c(n,t){return"註冊頁"}const o=e(r,[["render",c]]);export{o as default};
